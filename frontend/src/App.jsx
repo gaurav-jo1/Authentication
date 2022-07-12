adf48df76f21adf83339731da37cea41e53e6573
Gaurav Joshi <gj451026@gmail.com>
2022-07-12 11:48:27 +0530

    Added Header for Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
+import Header from "./components/Header";
+
 // Styling
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
