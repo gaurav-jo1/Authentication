9f5be593175e6e7edb8e709c6121a1bd56cc225f
Gaurav Joshi <gj451026@gmail.com>
2022-07-12 11:39:38 +0530

    React router set up finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+// Tools
+import { Route, Routes } from "react-router-dom";
+
+// Pages
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+// Styling
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Namaste, World!</h1>
-      <p>Here is our react website</p>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" name="username" placeholder='Enter Username'/>
+            <input type="password" name="password" placeholder='Enter Password'/>
+            <input type="submit" />
+        </form>
+    </div>
+  )
+}
+
+export default LoginPage
