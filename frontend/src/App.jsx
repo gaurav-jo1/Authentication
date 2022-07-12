9669fd95f14f8408ee8e67116ae94e939b452642
Gaurav Joshi <gj451026@gmail.com>
2022-07-12 10:54:31 +0530

    frontend basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Namaste, World!</h1>
+      <p>Here is our react website</p>
+    </div>
+  );
+}
+
+export default App;
