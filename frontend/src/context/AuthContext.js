39f889dc045fab06352ef52236b2bb004a5435b3
Gaurav Joshi <gj451026@gmail.com>
2022-07-19 17:44:20 +0530

    login request with using axios completed

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -6,11 +6,13 @@ const AuthContext = createContext();
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-  let [authTokens, setAuthTokens] = useState(() => localStorage.getItem("authTokens") ? JSON.parse(localStorage.getItem("authTokens")) : null );
-  let [user, setUser] = useState(() => localStorage.getItem("authTokens") ? jwt_decode(localStorage.getItem("authTokens")) : null );
+  let [authTokens, setAuthTokens] = useState(() => localStorage.getItem("authTokens") ? JSON.parse(localStorage.getItem("authTokens")) : null);
+  let [user, setUser] = useState(() => localStorage.getItem("authTokens") ? jwt_decode(localStorage.getItem("authTokens")) : null);
   let [loading, setLoading] = useState(true);
 
-   //   Login User
+
+
+  //   Login User
 
   let loginUser = async (e) => {
     e.preventDefault();
@@ -35,7 +37,7 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-   // Logout User
+  // Logout User
 
   let logoutUser = () => {
     setAuthTokens(null);
@@ -43,57 +45,29 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("authTokens");
   };
 
-  let updateToken = async () => {
-    let response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ 'refresh':authTokens?.refresh }),
-    });
-    let data = await response.json();
-
-    if (response.status === 200) {
-      setAuthTokens(data);
-      setUser(jwt_decode(data.access));
-      localStorage.setItem("authTokens", JSON.stringify(data));
-    } else {
-      logoutUser()
-    } 
-
-    if(loading){
-      setLoading(false);
-    }
-  }
-
-   //   State management 
+  //   State management 
 
   let contextData = {
     user: user,
     authTokens: authTokens,
+    setAuthTokens: setAuthTokens,
+    setUser: setUser,
     loginUser: loginUser,
     logoutUser: logoutUser,
   };
 
   useEffect(() => {
 
-    if(loading){
-      updateToken();
+    if (authTokens) {
+      setUser(jwt_decode(authTokens.access))
     }
-
-    let fourMinutes = 1000 * 60 * 4;
-    let interval = setInterval(() => {
-      if(authTokens) {
-        updateToken()
-      }
-    }, fourMinutes)
-    return () => clearInterval(interval)
+    setLoading(false);
 
   }, [authTokens, loading])
 
   return (
     <AuthContext.Provider value={contextData}>
-      { loading ? null : children }
+      {loading ? null : children}
     </AuthContext.Provider>
   );
 };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,40 +1,33 @@
-import React, {useState, useEffect, useContext} from "react";
-import AuthContext from "../context/AuthContext";
+import React, { useState, useEffect } from "react";
+import useAxios from "../utils/useAxios";
 
-import './HomePage.scss';
+import "./HomePage.scss";
 
 const HomePage = () => {
   let [notes, setNotes] = useState([]);
-  let {authTokens, logoutUser} = useContext(AuthContext);
-  
-  useEffect(()=> {
-    getNotes()
-}, [])
 
-  let getNotes = async() =>{
-    let response = await fetch('http://127.0.0.1:8000/api/notes/', {
-        method:'GET',
-        headers:{
-            'Content-Type':'application/json',
-            'Authorization':'Bearer ' + String(authTokens.access)
-        }
-    })
-    let data = await response.json()
+  let api = useAxios();
 
-    if(response.status === 200){
-        setNotes(data)
-    }else if(response.statusText === 'Unauthorized'){
-        logoutUser()
+  useEffect(() => {
+    getNotes();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let getNotes = async () => {
+    let response = await api.get("/api/notes/");
+
+    if (response.status === 200) {
+      setNotes(response.data);
     }
-    
-}
+  };
 
   return (
     <div className="HomePage_container">
       <p>You are logged to the home page</p>
 
       <ul>
-        {notes.map(note => (
+        {notes.map((note) => (
           <li key={note.id}>{note.body}</li>
         ))}
       </ul>
