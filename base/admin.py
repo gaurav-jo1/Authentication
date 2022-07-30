ca97b3a1ea55a16f59d7276037a904292c0af88c
Gaurav Joshi <gj451026@gmail.com>
2022-07-30 17:50:32 +0530

    deleted base folder

--- base/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Note
-# Register your models here.
-
-admin.site.register(Note)
--- base/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from pyexpat import model
-from rest_framework.serializers import ModelSerializer
-from base.models import Note
-
-class NoteSerializers(ModelSerializer):
-    class Meta:
-        model = Note
-        fields = '__all__'
